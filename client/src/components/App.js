acb3ddaad7a918a64990bb21975d552782fd11cc
Sudarshan Suryaprakash <sudarshan@Sudarshans-MacBook-Air.local>
2020-10-28 12:44:11 +0000

    wed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,6 @@ import { Router, Route, Switch } from 'react-router-dom';
 
 import StreamList from './streams/StreamList';
 import StreamEdit from './streams/StreamEdit';
-import StreamDelete from './streams/StreamDelete';
 import StreamCreate from './streams/StreamCreate';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
@@ -20,7 +19,6 @@ const App = () => {
           <Route path='/streams/new' exact component={StreamCreate} />
           <Route path='/streams/:id' exact component={StreamShow} />
           <Route path='/streams/edit/:id' exact component={StreamEdit} />
-          <Route path='/streams/delete' exact component={StreamDelete} />
           </Switch>
         </div>
       </Router>
